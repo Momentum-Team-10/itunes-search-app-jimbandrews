cef3c7309772abc922b8152ea5b1c57f8fedd17e
James Andrews <jbandrew14@gmail.com>
2021-10-19 08:33:57 -0400

    search query returns only songs for searched band and displays album artwork, limited to 10 results

--- main.js
+++ main.js
@@ -1,25 +1,32 @@
 const form = document.getElementById("music-form");
 const url = "https://itunes.apple.com/search?term=";
-const infoKeys = ["artworkUrl100", "trackName", "collectionName", "artistName", "releaseDate", "previewUrl"];
+const resultsLimit = "&entity=song&attribute=artistTerm&limit=10"
+const infoKeys = ["trackName", "collectionName", "artistName", "releaseDate", "previewUrl"];
 const results = document.getElementById("search-results");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     let input = document.getElementById("music-query");
     let artist = input.value.toLowerCase().replace(' ', '+');
-    console.log(artist);
-    fetch(url+artist)
-        .then(res => res.json)
+    fetch(url+artist+resultsLimit)
+        .then(res => res.json())
         .then(data => {
-            for (let item of data) {
-                let songCard = document.createElement("ul");
-                fillSongCard(item, songCard)
-                results.appendChild(songCard)
+            for (let item of data.results) {
+                showSongCard(item)
             }
         })
 })
 
 
 function fillSongCard(songObj, songCard) {
-    
+    let albumArt = document.createElement("img");
+    albumArt.src = songObj.artworkUrl100;
+    songCard.appendChild(albumArt);
+
+}
+
+function showSongCard(songObj) {
+    let songCard = document.createElement("div");
+    fillSongCard(songObj, songCard)
+    results.appendChild(songCard)
 }
